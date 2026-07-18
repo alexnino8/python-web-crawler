08f633db0a7dab6f86366285058bb87f8959a3bf
alexnino8 <alopeznino98@gmail.com>
2026-07-18 13:01:43 -0600

    implement get_heading_from_html func and tests

--- crawl.py
+++ crawl.py
@@ -1,6 +1,10 @@
 import urllib.parse
+from bs4 import BeautifulSoup, Tag
 
-def normalize_url(url):
+
+
+# this function normalizes the url provided stripping the protocol and any trailing slashes
+def normalize_url(url: str) -> str:
     url_obj = urllib.parse.urlsplit(url)
     netloc = url_obj.netloc
     path = url_obj.path
@@ -11,5 +15,18 @@ def normalize_url(url):
     normalized_url = netloc + path
 
     return normalized_url
+
+# this function will return the h1 with fallback to h2 
+def get_heading_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+
+    heading = ""
+    if soup.h1 != None:
+        heading = str(soup.h1.string)
+    else:
+        if soup.h2 != None:
+            heading = str(soup.h2.string)
+
+    return heading
 
 
--- test_crawl.py
+++ test_crawl.py
@@ -1,7 +1,7 @@
 import unittest
-from crawl import normalize_url
+from crawl import normalize_url, get_heading_from_html
 
-class TestCrawl(unittest.TestCase):
+class TestNormalizeUrl(unittest.TestCase):
     def test_normalize_url_with_https(self):
         input_url = "https://www.boot.dev/blog/path"
         actual = normalize_url(input_url)
@@ -24,6 +24,46 @@ class TestCrawl(unittest.TestCase):
         input_url = "https://www.boot.dev/"
         actual = normalize_url(input_url)
         expected = "www.boot.dev"
+        self.assertEqual(actual, expected)
+
+class TestExtractHTMLHeading(unittest.TestCase):
+    def test_extract_h1_basic(self):
+        html_doc = '''
+            <html>
+                <body>
+                    <h1>Test Title</h1>
+                </body>
+            </html>
+
+        '''
+        actual = get_heading_from_html(html_doc)
+        expected = "Test Title"
+        self.assertEqual(actual, expected)
+
+    def test_extract_h2_fallback(self):
+        html_doc = '''
+            <html>
+                <body>
+                    <h2>Test Fallback when no h1.</h2>
+                </body>
+            </html>
+
+        '''
+        actual = get_heading_from_html(html_doc)
+        expected = "Test Fallback when no h1."
+        self.assertEqual(actual, expected)
+
+    def test_extract_nothing(self):
+        html_doc = '''
+            <html>
+                <body>
+                    <p>Paragraph only</p>
+                </body>
+            </html>
+
+        '''
+        actual = get_heading_from_html(html_doc)
+        expected = ""
         self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
